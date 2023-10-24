16c1f309408b034faadaf5928ba2bd5124ffde88
Mykola <95630245+miowff@users.noreply.github.com>
2023-10-24 15:00:50 +0300

    Code name fixed

--- schema/phoneNumberCode.ts
+++ schema/phoneNumberCode.ts
@@ -2,7 +2,7 @@ import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 export const phoneNumberCode = mysqlTable("PhoneNumberCode", {
   phoneNumber: varchar("PhoneNumber", { length: 100 }).notNull(),
-  code: int("Price").notNull().default(5),
+  code: int("Code").notNull(),
   resendTries: int("ResendTries").notNull().default(1),
   sentTime: int("SentTime").notNull(),
 });
