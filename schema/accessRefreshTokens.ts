8a2f87b802c3557a92a8422f1e6c1030c3211dd2
Mykola <95630245+miowff@users.noreply.github.com>
2023-11-03 15:08:07 +0200

    removed access token field

--- schema/accessRefreshTokens.ts
+++ schema/accessRefreshTokens.ts
@@ -1,10 +1,9 @@
 import { mysqlTable, varchar } from "drizzle-orm/mysql-core";
 import { users } from "./user";
 
-export const accessRefreshTokens = mysqlTable("AccessRefreshTokens", {
+export const userRefreshToken = mysqlTable("UserRefreshToken", {
   userId: varchar("UserId", { length: 80 })
     .references(() => users.id)
     .notNull(),
-  accessToken: varchar("AccessToken", { length: 256 }).notNull(),
   refreshToken: varchar("RefreshToken", { length: 256 }).notNull(),
 });
